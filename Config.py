b14cffbe15f3f33af5404308864a8b6e897bafbc
asad31 <nemo310397@gmail.com>
2025-07-15 01:19:50 -0400

    Adding files, folders, and fonts

---
+++ Config.py
@@ -0,0 +1,21 @@
+Config = {
+    "game": {
+        "caption": "Snake",
+        "width": 800,
+        "height": 600,
+        "fps": 20,
+        "bumper_size": 20,
+        "font": "./VT323-Regular.ttf",
+        "title-font": "./PressStart2P-Regular.ttf",
+    },
+    "snake": {"height": 10, "width": 10, "speed": 10},
+    "apple": {"width": 10, "height": 10},
+    "colors": {
+        "white": (255, 255, 255),
+        "black": (70, 65, 65),
+        "apple-red": (255, 0, 0),
+        "red": (200, 95, 30),
+        "green": (160, 180, 50),
+        "blue": (60, 100, 150),
+    },
+}
